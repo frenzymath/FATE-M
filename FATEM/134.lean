c242f594f2c934be5383559303c91ade4baac33c
Jiedong Jiang <emailboxofjjd@163.com>
2026-02-23 03:02:02 +0800

    fix: M-15 and M-134

--- FATEM/134.lean
+++ FATEM/134.lean
@@ -1,7 +1,9 @@
 import Mathlib
 
-variable [Group G] [Finite G]
-
--- Every Sylow $p$ group of $G$ is finite.
-instance : Finite (Sylow p G) := by
+/--
+Show that if $H$ is a normal subgroup of $G$ and $H$ is a $p$-group, then $H$ is contained in
+every Sylow $p$-subgroup of $G$.
+-/
+theorem Sylow.le_of_normal {G : Type*} [Group G] [Finite G] (H : Subgroup G) [H.Normal] (p : ℕ)
+    (hp : p.Prime) (hH : IsPGroup p H) (P : Sylow p G) : H ≤ P := by
   sorry
